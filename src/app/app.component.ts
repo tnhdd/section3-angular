d837bc06a1e9de3e6d4c71d4deaf69f552e5f7bd
tnhdd <tuandod21@gmail.com>
2024-11-26 13:58:33 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../public/dummy-users';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'section3';
+
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+}
---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  enteredTtitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
+  private tasksService = inject(TasksService);
+
+  onCancel() {
+    this.close.emit();
+  }
+  onSubmit() {
+    this.tasksService.addTask(
+      {
+        title: this.enteredTtitle,
+        summary: this.enteredSummary,
+        date: this.enteredDate,
+      },
+      this.userId
+    );
+    this.close.emit();
+  }
+}
